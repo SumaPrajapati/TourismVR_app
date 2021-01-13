842f1cbc3418e91d7d384038686a31a95054a89d
SumaPrajapati <sumap.dotnet@gmail.com>
2021-01-12 20:58:51 -0500

    Tourism VR app commit

---
+++ client.js
@@ -0,0 +1,135 @@
+// This file contains the boilerplate to execute your React app.
+// If you want to modify your application's content, start in "index.js"
+
+import {ReactInstance, Surface, Module} from 'react-360-web';
+
+function init(bundle, parent, options = {}) {
+  //const r360 = new ReactInstance(bundle, parent, {
+  r360 = new ReactInstance(bundle, parent, {
+    // Add custom options here
+    fullScreen: true,
+    nativeModules:[
+      new surfaceModule()
+    ],
+    ...options,
+  });
+
+  introPanel = new Surface(
+    500,
+    400,
+    Surface.SurfaceShape.Cylinder
+  );
+
+  introRoot = r360.renderToSurface(
+    r360.createRoot('TourismAppVR', {}),
+    introPanel
+  );
+
+  marketPanel = new Surface(
+    100,
+    100,
+    Surface.SurfaceShape.Flat
+  )
+
+  marketPanel.setAngle(
+    0.2,
+    0
+  );
+
+  museumPanel = new Surface(
+    100,
+    100,
+    Surface.SurfaceShape.Flat
+  )
+
+  museumPanel.setAngle(
+    Math.PI / 2,
+    0
+  );
+
+  restaurantPanel = new Surface(
+    100,
+    100,
+    Surface.SurfaceShape.Flat
+  )
+
+  restaurantPanel.setAngle(
+    -Math.PI / 2,
+    0
+  );
+
+  shoppingPanel = new Surface(
+    100,
+    100,
+    Surface.SurfaceShape.Flat
+  );
+
+ /*  r360.renderToSurface(
+    r360.createRoot('InfoPanel',{}),
+    marketPanel
+  );
+
+  r360.renderToSurface(
+    r360.createRoot('InfoPanel',{}),
+    shoppingPanel
+  );
+
+  r360.renderToSurface(
+    r360.createRoot('InfoPanel',{}),
+    museumPanel
+  );
+
+  r360.renderToSurface(
+    r360.createRoot('InfoPanel',{}),
+    restaurantPanel
+  ); */
+
+  shoppingPanel.setAngle(
+    3.6,
+    0
+  );
+
+  r360.compositor.setBackground(r360.getAssetURL('gdansk.jpg'));
+}
+
+class surfaceModule extends Module{
+  constructor(){
+    super('surfaceModule');
+  }
+
+  resizeSurface(width, height, id){
+    if(id === 'museum'){
+      museumPanel.resize(width, height);
+    } else if (id === 'restaurant'){
+      restaurantPanel.resize(width, height);
+    } else if (id === 'shopping'){
+      shoppingPanel.resize(width, height);
+    } else if (id === 'market'){
+      marketPanel.resize(width, height);
+    }
+  }
+  start(){
+    r360.renderToSurface(
+      r360.createRoot('InfoPanel',{id:'market', text: 'Browse our incredible market' }),
+      marketPanel
+    );
+  
+    r360.renderToSurface(
+      r360.createRoot('InfoPanel',{id:'shopping', text: 'Shop until you drop!'}),
+      shoppingPanel
+    );
+  
+    r360.renderToSurface(
+      r360.createRoot('InfoPanel',{id:'museum', text: 'The Life of Pablo Piccasso: Blue.'}),
+      museumPanel
+    );
+  
+    r360.renderToSurface(
+      r360.createRoot('InfoPanel',{id:'restaurant', text: 'Enjoy a delecious beer at our restaurants. '}),
+      restaurantPanel
+    );
+    r360.detachRoot(introRoot);
+  }
+}
+
+window.React360 = {init};
